32b0c63850f2a54cd916e46c88547f0d2d820fc9
Alexander Specht <afspecht02@gmail.com>
2023-10-09 23:17:18 -0700

    reset prices

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,17 +13,19 @@ def get_catalog():
     """
     
     with db.engine.begin() as connection:
-        currPotions = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory"))
+        currPotions = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions, num_dark_potions FROM global_inventory"))
         first_row = currPotions.first()
         
         redPotions = first_row.num_red_potions
         greenPotions = first_row.num_green_potions
         bluePotions = first_row.num_blue_potions
+        darkPotions = first_row.num_blue_potions
 
     potionTypes = {}
-    potionTypes['red'] = {'sku':'RED_POTION_0', 'name': 'red potion', 'quantity': redPotions // 2, 'price': 1, 'potion_type': [100, 0, 0, 0]}
-    potionTypes['green'] = {'sku': 'GREEN_POTION_0', 'name': 'green_potion', 'quantity': greenPotions // 2, 'price': 1, 'potion_type': [0, 100, 0, 0]}
-    potionTypes['blue'] = {'sku': 'BLUE_POTION_0', 'name': 'blue_potion', 'quantity': bluePotions // 2, 'price': 1, 'potion_type': [0, 0, 100, 0]}
+    potionTypes['red'] = {'sku':'RED_POTION_0', 'name': 'red potion', 'quantity': redPotions // 2, 'price': 20, 'potion_type': [100, 0, 0, 0]}
+    potionTypes['green'] = {'sku': 'GREEN_POTION_0', 'name': 'green_potion', 'quantity': greenPotions // 2, 'price': 20, 'potion_type': [0, 100, 0, 0]}
+    potionTypes['blue'] = {'sku': 'BLUE_POTION_0', 'name': 'blue_potion', 'quantity': bluePotions // 2, 'price': 30, 'potion_type': [0, 0, 100, 0]}
+    potionTypes['dark'] = {'sku': 'DARK_POTION_0', 'name': 'dark_potion', 'quantity': darkPotions // 2, 'price': 30, 'potion_type': [0, 0, 100, 0]}
 
     potionCatalog = []
     for potion in potionTypes:
