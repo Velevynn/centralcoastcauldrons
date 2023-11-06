ea732323b971ce8a00d6b4f78c6806256a8382f4
Velevynn <afspecht02@gmail.com>
2023-11-06 10:55:20 -0800

    100000 ml breakpoint

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -102,7 +102,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 mlBreakpoint = 15000
         elif 'LARGE' in barrel.sku:
             largeCatalog = True
-            mlBreakpoint = 60000
+            mlBreakpoint = 100000
         if 'MINI' not in barrel.sku:
             barrelDict[barrel.sku] = barrel
                 
