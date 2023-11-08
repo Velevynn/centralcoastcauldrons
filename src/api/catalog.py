dfe8231f2b5cfa4fff916a9ab5622906511bd473
Velevynn <afspecht02@gmail.com>
2023-11-08 14:18:30 -0800

    don't catalog new potions yet

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -14,14 +14,16 @@ def get_catalog():
     
     with db.engine.begin() as connection:
         # Create sliding view to offer potions according to what has been sold recently
-        # sql = (
-        #     """
-        #         CREATE VIEW potions_to_sell AS
-        #             WITH recent_transactions AS (
-        #                 SELECT 
-        #             )
-        #     """
-        #     )
+        sql = (
+            """
+                CREATE VIEW potions_to_sell AS
+                    WITH recent_transactions AS (
+                        SELECT potion_id
+                        FROM potion_ledger
+                        WHERE created_at 
+                    )
+            """
+            )
         result = connection.execute(sqlalchemy.text(
                                                 """
                                                     SELECT potions.potion_id, item_sku, potions.name, red_ml, green_ml, blue_ml, dark_ml, price, COALESCE(SUM(change), 0)::int AS quantity
@@ -34,7 +36,7 @@ def get_catalog():
         potionCatalog = []
         
         for potion in potionTable:
-            if potion.potion_id >= 12:
+            if potion.potion_id >= 12 or len(potionCatalog) == 6:
                 continue
             if potion.quantity > 0:
                 potionCatalog.append(
